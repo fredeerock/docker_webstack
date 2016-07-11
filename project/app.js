4ab311ee972640f02ac89d1fd19f2d468eae2429
Derick Ostrenko <dostrenko@gmail.com>
2016-07-11 13:42:50 +1200

    Update app.js

--- project/app.js
+++ project/app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function (req, res) {
   res.writeHead(200);
-  res.end('Hello Kubernetes!');
+  res.end('Hello World!');
 };
 var www = http.createServer(handleRequest);
 www.listen(process.env.PORT || 8080);
